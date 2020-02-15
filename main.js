af44b635f6cd4efbfeb917fdcfe638aba7aebbe9
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-02-15 15:23:29 -0800

    function tests for palindrom

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-function isPalindome (userInput) {
+function isPalindrome(userInput) {
     let reverseString = ""
-    for (let i = 0; i >= 0; i++) {
-        let lastLetter = userInput.length - 1 - 1;
-        let firstLetter = userInput[1]
+    for (let i = userInput.length - 1; i >= 0; i--) {
+        // let lastLetter = userInput.length - 1 - 1;
+        // let firstLetter = userInput[1]
         reverseString += userInput[i]
-        
     }
+
+    console.log(reverseString)
+
     if (userInput === reverseString) {
+        console.log("palindrome tested true")
         return true;
     }
+    console.log("palindrome tested false")
     return false;
+}
+
+
+function testFunc() {
+    const inputField = document.getElementById("input").value;
+    
+    console.log(inputField)
+    const me = isPalindrome(inputField)
+    
+    if (me === true) {
+        console.log("is palindrome")
+       
+    }
+    else {
+        console.log("not palindrome")
+    }
 }
