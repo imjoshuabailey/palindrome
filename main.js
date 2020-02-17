5eba989e810da7050c54e3db249448c305d69e6f
marcusjacinto <marcusjacinto@gmail.com>
2020-02-16 17:23:26 -0800

    Started function for character checking

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 function isPalindome (userInput) {
     let reverseString = ""
     for (let i = 0; i >= 0; i++) {
-        let lastLetter = userInput.length - 1 - 1;
-        let firstLetter = userInput[1]
-        reverseString += userInput[i]
+        let lastLetter = userInput.length - 1 - i;
+        let firstLetter = userInput[i]
+        
         
     }
     if (userInput === reverseString) {
         return true;
+        console.log("Is a Palindrome");
     }
     return false;
+    console.log("Is not a Palindrome")
 }
+
+/*
+function checkInp(userInput)
+ {
+    let letters= /^[a-zA-Z]+$/;
+    if (userInput.value.match(letters))
+    {
+    return true;
+    console.log ("All are Letters");
+    }
+    else
+    {
+    alert ("Please input alaphabet characters only");
+    return false;
+    console.log ("Non standard alphabet character detected");
+    }
+}
+*/
