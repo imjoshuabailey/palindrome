f775d0586cf0cc1e54eef7a86eda09b70ecd15c1
marcusjacinto <marcusjacinto@gmail.com>
2020-02-16 19:26:16 -0800

    finalized javascript funtions

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
 function isPalindrome(userInput) {
     let reverseString = ""
-<<<<<<< HEAD
-    for (let i = 0; i >= 0; i++) {
-        let lastLetter = userInput.length - 1 - i;
-        let firstLetter = userInput[i]
-        
-        
-=======
+
     for (let i = userInput.length - 1; i >= 0; i--) {
-        // let lastLetter = userInput.length - 1 - 1;
+        // let lastLetter = userInput.length - 1 - i;
         // let firstLetter = userInput[1]
         reverseString += userInput[i]
->>>>>>> 551992764bb171ec3ba2b068022e7ad8e576312c
     }
 
     console.log(reverseString)
@@ -19,32 +12,27 @@ function isPalindrome(userInput) {
     if (userInput === reverseString) {
         console.log("palindrome tested true")
         return true;
-        console.log("Is a Palindrome");
     }
     console.log("palindrome tested false")
     return false;
-    console.log("Is not a Palindrome")
 }
 
-/*
+
 function checkInp(userInput)
  {
     let letters= /^[a-zA-Z]+$/;
-    if (userInput.value.match(letters))
+    if (userInput.match(letters))
     {
-    return true;
     console.log ("All are Letters");
+    return true;
     }
     else
     {
-    alert ("Please input alaphabet characters only");
+    console.log("There are non-letters");
     return false;
-    console.log ("Non standard alphabet character detected");
     }
 }
-<<<<<<< HEAD
-*/
-=======
+
 
 
 
@@ -52,15 +40,38 @@ function insertText() {
     const inputField = document.getElementById("input").value;
     
     console.log(inputField)
-    const me = isPalindrome(inputField)
-   
-    if (me === true) {
+    const checking = checkInp(inputField)
+
+    console.log(checking)
+    
+
+    let palindromeCheck= "";
+
+    if (checking) {
+        palindromeCheck = isPalindrome(inputField)
+    }
+    else {
+        alert ("Please input alaphabet characters only");
+        console.log ("Non standard alphabet character detected");
+        return false;
+    }
+
+    if (palindromeCheck === true) {
         console.log("is palindrome")
-      
+        let ul = document.getElementById("isPalindrome"); 
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(inputField));
+        ul.appendChild(li);
     }
     else {
         console.log("not palindrome")
-      
+        let ul = document.getElementById("notPalindrome"); 
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(inputField));
+        ul.appendChild(li);
     }
 }
->>>>>>> 551992764bb171ec3ba2b068022e7ad8e576312c
+
+let form = document.getElementById("inputForm");
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm); 
